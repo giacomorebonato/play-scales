98b6eb51efa588e59b3e27ab2da5950ac50bf3b2
Giacomo Rebonato <grebonato@outlook.ie>
2022-04-12 14:34:44 +0200

    - configured jest - updated next - moved code to ./src

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,13 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json'
-    }
-  },
-  coverageReporters: ['json-summary', 'text'],
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts']
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({ dir: './' })
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
 }
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(
+  customJestConfig
+)
